4d128ba1abb36bca88b8e666d5bfec42933fabd7
Moe.Nishiguchi <n221t126@st.kyoto-seika.ac.jp>
2023-08-03 15:09:26 +0900

    変更

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+let isActive = true;
+let dialogBase = document.getElementsByClassName('dialog-base')[0];
+	console.log(dialogBase);
+
+function openDialog() {
+	dialogBase.removeAttribute("hidden");
+}
+
+function closeDialog() {
+	dialogBase.setAttribute("hidden", "hidden");
+}
+
+
 import anime from 'animejs/lib/anime.es.js';
 
 anime({
